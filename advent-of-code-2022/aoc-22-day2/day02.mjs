19c6cbe99765d1b22b28d536c4101647751f1369
Cynthia Mbulu <72040367+CyntiBinti@users.noreply.github.com>
2022-12-09 14:40:56 +0000

    updated README

--- advent-of-code-2022/aoc-22-day2/day02.mjs
+++ advent-of-code-2022/aoc-22-day2/day02.mjs
@@ -6,54 +6,65 @@ const gameStrategy = readFileSync("day02-input.txt", { encoding: "utf-8" }) // r
   .trim() // Remove starting/ending whitespace
   .split("\n"); // Split where it finds a single new line and create a 2D substring array (at the point of the single newline)
   
-  let totalScore = 0
+  let totalScore = 0;
+
   const rock = {
-    letter: "A" || "X",
+    opponentLetter: "A",
+    playerLetter: "X",
     score: 1
   }
 
   const paper = {
-    letter: "B" || "Y",
+    opponentLetter: "B",
+    playerLetter: "Y",
     score: 2
   }
 
   const scissors = {
-    letter: "C" || "Z",
+    opponentLetter: "C",
+    playerLetter: "Z",
     score: 3
   }
 
   const result = (opponent, player) => {
-    console.log(`opponent is: ${opponent}`)
-    console.log(`player is: ${player}`)
-    if ((opponent === rock.letter && player === scissors.letter) || 
-        (opponent === paper.letter && player === rock.letter) || 
-        (opponent === scissors.letter && player === paper.letter)) {
-        console.log(`=========1`)
-        return totalScore +=0
+    // if player wins
+    if ((player === rock.playerLetter && opponent === scissors.opponentLetter) || 
+        (player === paper.playerLetter && opponent === rock.opponentLetter) || 
+        (player === scissors.playerLetter && opponent === paper.opponentLetter)) {
+        return totalScore +=6;
     }
-    if ((player === rock.letter && opponent === scissors.letter) || 
-        (player === paper.letter && opponent === rock.letter) || 
-        (player === scissors.letter && opponent === paper.letter)) {
-        console.log(`=========2`)
-        return totalScore +=6
+    // if opponent wins
+    if ((opponent === rock.opponentLetter && player === scissors.playerLetter) || 
+        (opponent === paper.opponentLetter && player === rock.playerLetter) || 
+        (opponent === scissors.opponentLetter && player === paper.playerLetter)) {
+        return totalScore +=0;
     }
-    console.log(`=========3`)
-    return totalScore +=3
+    // if it's a tie
+    return totalScore +=3;
   }
 
-  console.log(`gameStrategy is: ${gameStrategy}`)
+  const playerScore = (player) => {
+    if (player === rock.playerLetter) {
+        return totalScore += rock.score;
+    }
+    if (player === paper.playerLetter) {
+        return totalScore += paper.score;
+    }
+    if (player === scissors.playerLetter) {
+        return totalScore += scissors.score;
+    }
+  }
 
 function part1() {
     const rounds = gameStrategy.map(round => {
-        console.log(`round is: ${round}`)
         const turn = round.split(" ")
-        console.log(`turn[0] is: ${turn[0]}`) 
-        console.log(`turn[1] is: ${turn[1]}`) 
  
-        result(turn[0], turn[1])
-        console.log(`totalScore is: ${totalScore}`)
+        result(turn[0], turn[1]);
+
+        playerScore(turn[1]);
     })
 
+    console.log(`totalScore is: ${totalScore}`);
 }
 
 function part2() {
