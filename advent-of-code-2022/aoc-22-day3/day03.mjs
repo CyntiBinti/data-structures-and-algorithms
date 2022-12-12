f0629e4899f66f3e22c3656adfd2cab3529a4323
CyntiBinti <cynthia_m@live.com>
2022-12-12 23:42:58 +0000

    advent of code 2022 day 3

--- advent-of-code-2022/aoc-22-day3/day03.mjs
+++ advent-of-code-2022/aoc-22-day3/day03.mjs
@@ -6,30 +6,47 @@ const allRucksackContents = readFileSync("day03-input.txt", { encoding: "utf-8" 
   .trim() // Remove starting/ending whitespace
   .split("\n"); // Split where it finds a single new line and create a 2D substring array (at the point of the single newline)
 
-  /*
-  Part1 task: find the common letter that appears in both compartments of each rucksack & then find its priority value. Sum all these values across all backpacks
-  */
+  const duplicateItem = [];
+  const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-  function rucksackCompartmentCheck(setA, setB) {
-    const rucksackCompartment = new Set(setA);
-    for (const item of setB) {
+
+  // using a hashMap, loop through compartmentB and check if compartmentA (the hashMap) has any duplicates. If so, then store the duplicate letter inside a variable to be used later
+  function rucksackCompartmentCheck(compartmentA, compartmentB) {
+    const rucksackCompartment = new Set(compartmentA);
+    for (const item of compartmentB) {
       if (rucksackCompartment.has(item)) {
-        const duplicateItem = item;
-        console.log(`item is: ${duplicateItem}`)
+        duplicateItem.push(item);
+        console.log(`duplicate item is: ${duplicateItem}`)
         return;
       }
     }
     return rucksackCompartment;
   }
 
+  // this function takes an input, and for each element it will push the index value of that element inside an array, and then a reduce function will sum the total of that array
+  function sumOfPriorityItems(duplicateItem) {
+    const priorityItemValues = [];
+    for (const item of duplicateItem) {
+      priorityItemValues.push((alphabet.indexOf(item) + 1))
+      console.log(`priorityItemValues are: ${priorityItemValues}`)
+      const priorityTotal = priorityItemValues.reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+      console.log(`priorityTotal is: ${priorityTotal}`)
+    }
+  }
+
 function part1() {
 
+  // loops through each rucksack and for each rucksack it splits the contents into 2 equal compartments
   const compartment = allRucksackContents.map(rucksack => {
     const halfARucksack = Math.round(rucksack.length/2);
     const compartment1 = rucksack.slice(0, halfARucksack);
     const compartment2 = rucksack.slice(halfARucksack)
 
-    rucksackCompartmentCheck(compartment1, compartment2)
+    // pass these newly created compartment halves into this function that will check for duplicate letters/items
+    rucksackCompartmentCheck(compartment1, compartment2);
+
+    // loops through the duplicateItem array and stores the index of each letter (when checked against the alphabet array) into a reducer function, to get the final sum
+    sumOfPriorityItems(duplicateItem);
 
   })
 }
