51f36f0bafde36f5bfecccbb35b1b0c0f1bb37f1
CyntiBinti <cynthia_m@live.com>
2022-12-12 22:36:05 +0000

    advent of code 2022 day 3

--- advent-of-code-2022/aoc-22-day3/day03.mjs
+++ advent-of-code-2022/aoc-22-day3/day03.mjs
@@ -7,19 +7,31 @@ const allRucksackContents = readFileSync("day03-input.txt", { encoding: "utf-8" 
   .split("\n"); // Split where it finds a single new line and create a 2D substring array (at the point of the single newline)
 
   /*
-  Part1 task: find the common letter that appears in both compartments of each rucksack & then find its priority value. Sum all these values
+  Part1 task: find the common letter that appears in both compartments of each rucksack & then find its priority value. Sum all these values across all backpacks
   */
 
+  function rucksackCompartmentCheck(setA, setB) {
+    const rucksackCompartment = new Set(setA);
+    for (const item of setB) {
+      if (rucksackCompartment.has(item)) {
+        const duplicateItem = item;
+        console.log(`item is: ${duplicateItem}`)
+        return;
+      }
+    }
+    return rucksackCompartment;
+  }
+
 function part1() {
-    const compartment = allRucksackContents.map(rucksack => {
-        const halfARucksack = Math.round(rucksack.length/2);
-        const compartment1 = rucksack.slice(0, halfARucksack);
-        const compartment2 = rucksack.slice(halfARucksack)
-        console.log(`compartment1 is: ${compartment1}`)
-        console.log(`compartment2 is: ${compartment2}`)
-    })
 
+  const compartment = allRucksackContents.map(rucksack => {
+    const halfARucksack = Math.round(rucksack.length/2);
+    const compartment1 = rucksack.slice(0, halfARucksack);
+    const compartment2 = rucksack.slice(halfARucksack)
 
+    rucksackCompartmentCheck(compartment1, compartment2)
+
+  })
 }
 
 function part2() {
@@ -28,3 +40,4 @@ function part2() {
  
 part1();
 part2();
+
