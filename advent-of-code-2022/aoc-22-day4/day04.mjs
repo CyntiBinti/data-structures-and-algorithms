7ccd5eaf937b691d6e006191f181aa724a40d8f6
CyntiBinti <cynthia_m@live.com>
2022-12-15 04:21:19 +0000

    advent of code 2022 day 4

---
+++ advent-of-code-2022/aoc-22-day4/day04.mjs
@@ -0,0 +1,37 @@
+import { readFileSync } from "node:fs";
+
+// takes in a text file and tidies it up via the readFileSync function
+const sectionPairs = readFileSync("day04-input.txt", { encoding: "utf-8" }) // read day??.txt content
+  .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
+  .trim() // Remove starting/ending whitespace
+  .split("\n"); // Split where it finds a single new line and create a 2D substring array (at the point of the single newline)
+
+  const overlapCounter = 0;
+
+function part1() {
+
+  const elfPair = sectionPairs.map(sectionPair => {
+    const aSection = sectionPair.toString().split(",");
+    const firstElfSection = aSection[0]
+    const secondElfSection = aSection[1]
+
+    const firstElf = firstElfSection.toString().split("-");
+    const firstElfSectionStartID = firstElf[0];
+    const firstElfSectionEndID = firstElf[1];
+
+    const secondElf = secondElfSection.toString().split("-");
+    const secondElfSectionStartID = secondElf[0];
+    const secondElfSectionEndID = secondElf[1];
+
+  })
+
+}
+
+
+function part2() {
+
+}
+ 
+part1();
+part2();
+
