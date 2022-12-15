20b9baf0b0151c8001271546afd4f65384442dce
CyntiBinti <cynthia_m@live.com>
2022-12-15 06:46:04 +0000

    advent of code 2022 day 4

--- advent-of-code-2022/aoc-22-day4/day04.mjs
+++ advent-of-code-2022/aoc-22-day4/day04.mjs
@@ -6,7 +6,8 @@ const sectionPairs = readFileSync("day04-input.txt", { encoding: "utf-8" }) // r
   .trim() // Remove starting/ending whitespace
   .split("\n"); // Split where it finds a single new line and create a 2D substring array (at the point of the single newline)
 
-  let overlapCounter = 0;
+  let part1OverlapCounter = 0;
+  let part2OverlapCounter = 0;
 
 function part1() {
 
@@ -25,17 +26,40 @@ function part1() {
 
     if ((firstElfSectionStartID <= secondElfSectionStartID) && (firstElfSectionEndID >= secondElfSectionEndID) ||
     (firstElfSectionStartID >= secondElfSectionStartID) && (firstElfSectionEndID <= secondElfSectionEndID )) {
-      overlapCounter ++
+      part1OverlapCounter ++
     }
 
   })
-  console.log(`overlapCounter is: ${overlapCounter}`)
-  return overlapCounter;
+  console.log(`part1OverlapCounter is: ${part1OverlapCounter}`)
+  return part1OverlapCounter;
 
 }
 
 
 function part2() {
+
+  const elfPair = sectionPairs.map(sectionPair => {
+    const aSection = sectionPair.toString().split(",");
+    const firstElfSection = aSection[0]
+    const secondElfSection = aSection[1]
+
+    const firstElf = firstElfSection.toString().split("-");
+    const firstElfSectionStartID = Number(firstElf[0]);
+    const firstElfSectionEndID = Number(firstElf[1]);
+
+    const secondElf = secondElfSection.toString().split("-");
+    const secondElfSectionStartID = Number(secondElf[0]);
+    const secondElfSectionEndID = Number(secondElf[1]);
+
+    if ((firstElfSectionStartID <= secondElfSectionStartID) && (firstElfSectionEndID >= secondElfSectionEndID) ||
+    (firstElfSectionStartID >= secondElfSectionStartID) && (firstElfSectionEndID <= secondElfSectionEndID ) ||
+    (firstElfSectionStartID <= secondElfSectionEndID) && (secondElfSectionStartID <= firstElfSectionEndID )) {
+      part2OverlapCounter ++
+    }
+
+  })
+  console.log(`overlapCounter is: ${part2OverlapCounter}`)
+  return part2OverlapCounter;
 
 }
  
