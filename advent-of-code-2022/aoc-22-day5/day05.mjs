7bf8ce6aca82ca31788b2ae38a781648d32f4610
CyntiBinti <cynthia_m@live.com>
2022-12-22 09:03:05 +0000

    advent of code day 5

--- advent-of-code-2022/aoc-22-day5/day05.mjs
+++ advent-of-code-2022/aoc-22-day5/day05.mjs
@@ -1,18 +1,33 @@
 import { readFileSync } from "node:fs";
 
 // takes in a text file and tidies it up via the readFileSync function
-const crateMovement = readFileSync("day05-input.txt", { encoding: "utf-8" }) // read day??.txt content
+const input = readFileSync("day05-input.txt", { encoding: "utf-8" }) // read day??.txt content
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
-  .trim() // Remove starting/ending whitespace
-  .split("\n") // Split where it finds a single new line and create a 2D substring array
+  .trimEnd(); // Remove ending whitespace
 
-  console.log(`1st move = ${crateMovement[0]}`)
-  console.log(`2nd move = ${crateMovement[1]}`)
-  console.log(`3rd move = ${crateMovement[2]}`)
-  console.log(`4th move = ${crateMovement[3]}`)
+const [stackInput, stackMoves] = input.split("\n\n").map(line => line.split("\n"));
 
-const stacks = [["Z", "N", "D"], ["M", "C"],["P"]]
-let topCrates = [];
+const parsedStacks = stackInput.map((line) =>
+  [...line].filter((value, index) => index % 4 === 1)
+);
+
+const stackIndexes = parsedStacks.pop();
+
+const stacks = {};
+
+for (const line of parsedStacks) {
+  for (let i = 0; i < line.length; i++) {
+    if (line[i] !== " ") {
+      if (!stacks[stackIndexes[i]]) {
+        stacks[stackIndexes[i]] = [];
+      }
+      stacks[stackIndexes[i]].unshift(line[i])
+    }
+    
+  }
+}
+
+console.log(stacks)
 
 function part1() {
 
@@ -24,12 +39,7 @@ function part1() {
         // where for each numberofCratesToMove, it pops the crate from the stackToMoveFrom, and then pushes it onto the stackToMoveTo
         // once the for loop is complete (ie no more numberofCratesToMove), then the for loop exits and it runs a new loop for the next line of instructions
     
-    // const crateMove = crateMovement.map(fullMove => {
-    //   const moveValues = fullMove.replace(/\D/g,'')
 
-    //   console.log(moveValues)
-      
-    // })
 
     // function2:
       // once all the moves have been made, loop through the stacks array and pop the last crate of each stack (i.e. stacks[i][stacks[i.length-1]]) and push it into a topCrates array
