fd735d09aa32e3771608a052ab89a5772162d0d8
CyntiBinti <cynthia.mbulu@ft.com>
2025-01-28 00:35:43 +0000

    updated solution for leetcode 242

--- leetcode/242-ValidAnagram.js
+++ leetcode/242-ValidAnagram.js
@@ -1,5 +1,7 @@
 /*
 
+LEVEL: EASY
+
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
@@ -11,38 +13,47 @@ Output: true
 Example 2:
 Input: s = "rat", t = "car"
 Output: false
- 
+
 Constraints:
-1 <= s.length, t.length <= 5 * 104
+1 <= s.length, t.length <= 5 * 10(power of 4)
 s and t consist of lowercase English letters
 
 */
 
-const isAnagram = (s, t) => {
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isAnagram = function (s, t) {
+	if (s.length !== t.length) {
+		return false;
+	}
 
-   if (s.length !== t.length) {
-    return false;
-   } 
-   else {
-        const sSortedArray = [...s].sort();
-        const tSortedArray = [...t].sort();
+	const sMap = new Map();
 
-        const tSortedSet = new Set(tSortedArray);
-        // realised using the Set object isn't best as doesnt make new entries duplicate values, so duplicates are filtered out
-        // and thus if the remaining chars across both arrays are the same then it gives a false positive
-        // TO-DO: instead use a Map object as this creates a new entry for duplicate values (rather than ignoring them)
+	for (const char of s) {
+		sMap.set(char, (sMap.get(char) || 0) + 1);
+	}
 
-        for (const letter of sSortedArray) {
-            if (!tSortedSet.has(letter)) {
-                return false;
-            }
-        } return true;
-   };
+	for (const char of t) {
+		if (!sMap.has(char)) {
+			return false;
+		}
 
+		sMap.set(char, sMap.get(char) - 1);
+
+		if (sMap.get(char) < 0) {
+			return false;
+		}
+	}
+
+	return true;
 };
 
-console.log(`input 1 answer is: ${isAnagram('anagram', 'nagaram')}`); // true
-console.log(`input 2 answer is: ${isAnagram('rat', 'car')}`); // false
-console.log(`input 3 answer is: ${isAnagram('ac', 'bb')}`); // false
-console.log(`input 4 answer is: ${isAnagram('aacc', 'ccac')}`); // false
-
+console.log(isAnagram('anagram', 'nagaram')); // true
+console.log(isAnagram('rat', 'car')); // false
+console.log(isAnagram('ac', 'bb')); // false
+console.log(isAnagram('aacc', 'ccac')); // false
+console.log(isAnagram('ggii', 'eekk')); // false
+console.log(isAnagram('fe', 'ja')); // false
