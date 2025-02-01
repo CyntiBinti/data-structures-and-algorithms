acd5232b17a439b97fcea106c2e25c663c5c5509
CyntiBinti <cynthia.mbulu@ft.com>
2025-02-01 19:15:31 +0000

    updated solution for leetcode 1 problem

--- leetcode/1-TwoSum.js
+++ leetcode/1-TwoSum.js
@@ -1,5 +1,7 @@
 /*
 
+LEVEL: EASY
+
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -18,36 +20,43 @@ Output: [1,2]
 Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
- 
 
 Constraints:
-2 <= nums.length <= 104
--109 <= nums[i] <= 109
--109 <= target <= 109
+2 <= nums.length <= 10(power of 4)
+-10(power of 9) <= nums[i] <= 10(power of 9)
+-10(power of 9) <= target <= 10(power of 9)
 Only one valid answer exists
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 
 */
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 const twoSum = function(nums, target) {
-    const updatedArray = [...nums].filter(num => num <= target)
+	const map = new Map();
 
-    const output = [];
+	// create a map of nums (key) and their associated index (value)
+	for (let i = 0; i < nums.length; i++) {
+		map.set(nums[i], i);
+	}
 
-    // loop through the array and check:
-    // if the length is === 2 then return the indices of those elements as they'd have to equal the target
-    if (updatedArray.length === 2) {
+	// loop through, find the diff, if that diff exists in the map and the index value of that diff
+	// isn't the same as the current index in the loop, then return the pair of indices
+	for (let i = 0; i < nums.length; i++) {
+		const diff = target - nums[i];
+		if (map.has(diff) && map.get(diff) !== i) {
+			return [i, map.get(diff)];
+		}
+	}
 
-        // TO-DO: need to refactor this so that can handle duplicate values as gives a false positive i.e [3,3] gives output [0,0] instead of [0,1]
-        for (let i = 0; i < updatedArray.length; i++) {
-            const value = updatedArray[i];
-            output.push(updatedArray.indexOf(value))
-        }
-        return output;
-    } return false
-    // else blah blah blah...
-
+	// else no pairs found, so return an empty array
+	return [];
 };
 
-console.log(`test1 is: ${twoSum([2, 7, 11, 15], 9)}`)
-console.log(`test2 is: ${twoSum([3, 2, 4], 6)}`)
-console.log(`test3 is: ${twoSum([3, 3], 6)}`)
+console.log(twoSum([2, 7, 11, 15], 9)) // [0,1]
+console.log(twoSum([3, 2, 4], 6)) // [1,2]
+console.log(twoSum([3, 3], 6)) // [0,1]
